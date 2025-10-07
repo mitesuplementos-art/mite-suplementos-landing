baa5b79fdb453dab05398fc191af1e2e10699de0
mitesuplementos-art <mitesuplementos@gmail.com>
2025-10-07 13:38:16 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-import "./globals.css";
-import { ReactNode } from "react";
-
-export const metadata = {
-  title: "Mite Suplementos",
-  description: "Landing Page da Mite Suplementos - Energia, Performance e Saúde",
-};
+import './globals.css'
+import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  );
+  )
 }
