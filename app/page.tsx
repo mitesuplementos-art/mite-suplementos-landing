d4bf1f1f8d3ff16faabfabc2b5d15cc9b0495bc2
mitesuplementos-art <mitesuplementos@gmail.com>
2025-10-09 11:40:23 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -264,9 +264,15 @@ Aguardo o contato!`
                       </li>
                     ))}
                   </ul>
-                  <button className="w-full bg-[#97E622] text-white py-3 rounded-lg font-semibold hover:bg-[#5A6B2A] transition-colors">
-                    Consultar Preço
-                  </button>
+                 <a
+  href="https://wa.me/5515989337295?text=Olá,%20quero%20consultar%20o%20preço"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button className="w-full bg-[#97E622] text-white py-3 rounded-lg font-semibold hover:bg-[#5A6B2A] transition-colors">
+    Consultar Preço
+  </button>
+</a>
                 </div>
               </div>
             ))}
