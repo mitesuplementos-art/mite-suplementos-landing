e0e7f15178b2a3c220fbc2e7414f60fdd3d327a9
mitesuplementos-art <mitesuplementos@gmail.com>
2025-10-10 16:21:33 -0300

    Update Analytics.tsx

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -1,24 +1,16 @@
 'use client'
 
-import { usePathname, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import * as gtag from '../lib/gtag'
 
 export default function Analytics() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
   useEffect(() => {
-    if (!pathname) return
-
     // Track inicial
-    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '') + window.location.hash
-    gtag.pageview(url)
+    gtag.pageview(window.location.href)
 
-    // Função para trackear hash changes
+    // Track hash changes
     const handleHashChange = () => {
-      const urlWithHash = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '') + window.location.hash
-      gtag.pageview(urlWithHash)
+      gtag.pageview(window.location.href)
     }
 
     window.addEventListener('hashchange', handleHashChange)
@@ -26,7 +18,7 @@ export default function Analytics() {
     return () => {
       window.removeEventListener('hashchange', handleHashChange)
     }
-  }, [pathname, searchParams])
+  }, [])
 
   return null
 }
