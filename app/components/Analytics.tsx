b10eed04034e3fcede751c8cf13a4a9f3649422c
mitesuplementos-art <mitesuplementos@gmail.com>
2025-10-10 15:52:01 -0300

    Create Analytics.tsx

---
+++ app/components/Analytics.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import * as gtag from '@/lib/gtag'
+
+export default function Analytics() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (!pathname) return
+    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '')
+    gtag.pageview(url)
+  }, [pathname, searchParams])
+
+  return null
+}
